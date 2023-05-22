9da4f29825fd5363e3c43f35792412fc4b4e550f
Pedro Henrique <pedrobarbosar12@gmail.com>
2023-05-22 11:57:39 -0300

    Primeiro Commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function somar() {
+    let tn1 = document.querySelector("#txtn1")
+    let tn2 = document.querySelector("#txtn2")
+
+    let res = document.querySelector("#res")
+
+    let n1 = Number(tn1.value)
+    let n2 = Number(tn2.value)
+
+    let s = n1 + n2
+
+    res.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
+}
+
